630f1da819a802c4a26881debdede69e2da7f163
ThinkTechSN <dev@thinktech.sn>
2018-01-04 22:22:40 +0000

    adding changes

--- thinktech.sql
+++ thinktech.sql
@@ -11,7 +11,21 @@ plan varchar(100), description text, priority varchar(30) default "normal" , dat
 
 drop table IF EXISTS tasks;
 
-create table tasks(id int AUTO_INCREMENT PRIMARY KEY,name varchar(200) not null,description text, priority varchar(30) default "normal" not null, date TIMESTAMP DEFAULT NOW(), status varchar(30) default "stand by" ,progression int default 0, project_id int not null);
+create table tasks(id int AUTO_INCREMENT PRIMARY KEY,name varchar(200) not null,description text);
+
+insert into tasks(name,description) values("Contrat et Caution","cette phase intiale établit la relation légale qui vous lie à ThinkTech");
+insert into tasks(name,description) values("Traitement","cette phase est celle où l'équipe technique prend en charge votre projet");
+insert into tasks(name,description) values("Analyse du projet","cette phase est celle de l'analyse de votre projet pour une meilleure compréhension des objectifs");
+insert into tasks(name,description) values("Définition des fonctionnalités","cette phase est celle de la définition des fonctionnalités du produit");
+insert into tasks(name,description) values("Conception de l'interface","cette phase est celle de la conception de l'interface utilisateur");
+insert into tasks(name,description) values("Développement des fonctionnalités","cette phase est celle du développement des fonctionnalités du produit");
+insert into tasks(name,description) values("Tests","cette phase permet de tester les fonctionnalités du produit");
+insert into tasks(name,description) values("Validation","cette phase est celle de la validation des fonctionnalités du produit");
+insert into tasks(name,description) values("Livraison","cette phase finale est celle de la livraison du produit");
+
+drop table IF EXISTS projects_tasks;
+
+create table projects_tasks(id int AUTO_INCREMENT PRIMARY KEY,priority varchar(30) default "normal" not null, date TIMESTAMP DEFAULT NOW(), status varchar(30) default "stand by" ,progression int default 0, task_id int not null,project_id int not null);
 
 drop table IF EXISTS projects_comments;
 
