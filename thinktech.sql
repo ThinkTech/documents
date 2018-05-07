2ccf3813d607a89ea55d949ec26c404980df07b2
Thinktech <dev@thinktech.sn>
2018-05-07 16:53:43 +0000

    improving the project.

--- thinktech.sql
+++ thinktech.sql
@@ -55,6 +55,10 @@ drop table IF EXISTS subscriptions;
 
 create table subscriptions(id bigint AUTO_INCREMENT PRIMARY KEY,service varchar(300) not null,structure_id bigint not null,per varchar(20) default "year",plan varchar(100),date TIMESTAMP DEFAULT NOW(),actived boolean default true,endedOn TIMESTAMP null);
 
+drop table IF EXISTS domains;
+
+create table domains(id bigint AUTO_INCREMENT PRIMARY KEY,name varchar(300) not null,email varchar(300),structure_id bigint not null);
+
 drop table IF EXISTS users;
 
 create table users(id bigint AUTO_INCREMENT PRIMARY KEY, name varchar(300) not null,email varchar(200) not null, password varchar(200) not null,
