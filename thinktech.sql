9f798a8f956e25cc731a597fce31cc52b2d8a2bb
Thinktech <dev@thinktech.sn>
2018-05-28 09:51:41 +0000

    improving the project.

--- thinktech.sql
+++ thinktech.sql
@@ -37,8 +37,8 @@ message text not null, date TIMESTAMP DEFAULT NOW(), unread boolean default true
 
 drop table IF EXISTS bills;
 
-create table bills(id bigint AUTO_INCREMENT PRIMARY KEY,fee varchar(200) not null,code varchar(200),
-date TIMESTAMP DEFAULT NOW(), status varchar(30) default "stand by",amount int not null,paidOn TIMESTAMP null,paidWith varchar(200),paidBy int,project_id bigint,discount_id bigint,group_id bigint,subscription_id bigint);
+create table bills(id bigint AUTO_INCREMENT PRIMARY KEY,fee varchar(200) not null,service varchar(100) not null,code varchar(200),
+date TIMESTAMP DEFAULT NOW(), status varchar(30) default "stand by",amount int not null,paidOn TIMESTAMP null,paidWith varchar(200),paidBy int,product_id bigint,discount_id bigint,group_id bigint);
 
 drop table IF EXISTS discounts;
 
