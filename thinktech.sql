a8e7dc3278d2cc4acd49321df8e4a3faa322c5f6
ThinkTech <dev@thinktech.sn>
2018-07-20 09:42:22 +0000

    improving the project.

--- thinktech.sql
+++ thinktech.sql
@@ -42,7 +42,7 @@ insert into structures(id,name) values(1,"ThinkTech");
 
 drop table IF EXISTS subscriptions;
 
-create table subscriptions(id bigint AUTO_INCREMENT PRIMARY KEY,service varchar(300) not null,structure_id bigint not null,date TIMESTAMP DEFAULT NOW(),actived boolean default true,endedOn TIMESTAMP null);
+create table subscriptions(id bigint AUTO_INCREMENT PRIMARY KEY,service varchar(300) not null,structure_id bigint not null,date TIMESTAMP DEFAULT NOW(),active boolean default true);
 
 drop table IF EXISTS domains;
 
